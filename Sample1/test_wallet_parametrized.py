cc0b45ae272339621b8f45e835858116fd71ea54
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-11 00:43:29 -0700

    Created fixture for test file

--- Sample1/test_wallet_parametrized.py
+++ Sample1/test_wallet_parametrized.py
@@ -3,13 +3,17 @@
 import pytest
 from wallet import Wallet, InsufficientAmount
 
+@pytest.fixture
+def my_wallet():
+	'''Returns an instance of Wallet with balance of 0'''
+	return Wallet()
+
 @pytest.mark.parametrize("earned,spent,expected", [
 	(30,10,20),
 	(20,2,18),
 	])
 
-def test_transactions(earned, spent, expected):
-	my_wallet = Wallet()
+def test_transactions(my_wallet,earned, spent, expected):
 	my_wallet.add_cash(earned)
 	my_wallet.spend_cash(spent)
 	assert my_wallet.balance == expected
